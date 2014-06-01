dc02b6bd226ca5e69f8a73d0669fa1d2d5c0eb39
mhakeem <mazhak@msn.com>
2014-06-01 03:42:41 -0600

    Added som execution code

--- celluloid_xkcd_printer.rb
+++ celluloid_xkcd_printer.rb
@@ -5,9 +5,26 @@ class XKCDFetcher
     include Celluloid
 
     def next
-        response = Net::HTTP.get_response('dynamic.xkcd.com', '/random/comic/')
+        response = Net::HTTP.get_response('dynamic.xkcd.com', 'random/comic/')
         random_comic_url = response['Location']
 
         random_comic_url
     end
+end
+
+fetcher = XKCDFetcher.new
+
+# puts fetcher.object_id
+# puts fetcher.inspect
+
+# puts fetcher.async.next
+# puts fetcher.async.next
+futures = []
+
+10.times do
+    futures << fetcher.future.next
+end
+
+futures.each do |future|
+    puts "You should check out #{future.value}"
 end
